3897026f8a96ae5bcd41b90e2f8eca9fd5f3ae4d
Abe4463 <abrahamle124@gmail.com>
2024-11-09 21:37:33 -0800

    added js

--- script.js
+++ script.js
@@ -5,18 +5,43 @@ function getComputerChoice() {
     return option[choice];
 }
 
-console.log(getComputerChoice());
-
 function getHumanChoice() {
     const answer = prompt('Please choose between rock paper or scissors');
     return answer.toLowerCase();
 }
 
-console.log(getHumanChoice());
-
 let humanScore = 0;
 let computerScore = 0;
 
-function playRound() {
-    
+function playRound(humanChoice, computerChoice) {
+    const human = humanChoice.toLowerCase();
+    const computer = computerChoice.toLowerCase();
+
+    if (human === computer) {
+        console.log(`Its a tie!`);
+    }
+    else if (human === 'rock' && computer === 'scissors') {
+        humanScore++;
+        console.log(`You win! ${human} beats ${computer}. Player: ${humanScore} Computer: ${computerScore}`);
+    } else if (human === 'paper' && computer === 'rock') {
+        humanScore++;
+        console.log(`You win. ${human} beats ${computer}. Player: ${humanScore} Computer: ${computerScore}`);
+    } else if (human === 'scissors' && computer === 'paper') {
+        humanScore++;
+        console.log(`You win. ${human} beats ${computer}. Player: ${humanScore} Computer: ${computerScore}`);
+    } else {
+        computerScore++;
+        console.log(`Computer wins. ${computer} beats ${human}. Player: ${humanScore} Computer: ${computerScore}`);
+    }
+}
+
+function playGame() {
+    const player = getHumanChoice();
+    const comp = getComputerChoice();
+
+    playRound(player, comp)
+}
+
+for (let i = 0; i <= 5; i++) {
+    console.log(playGame());
 }
