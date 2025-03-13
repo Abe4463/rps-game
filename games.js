c96dc50d313bf7d07a0b7f86a51444f8cb84d92c
Abe4463 <abrahamle124@gmail.com>
2025-03-12 23:05:11 -0700

    finished logic

--- games.js
+++ games.js
@@ -5,7 +5,53 @@ function getComputerChoice() {
 }
 
 function getHumanChoice() {
-    const playerChocie = prompt('Choose between rock, paper, or scissors');
-    return playerChocie;
+    const playerChoice = prompt('Choose between rock, paper, or scissors');
+    return playerChoice;
 }
 
+let playerScore = 0;
+let computerScore = 0;
+const humanSelection = getHumanChoice();
+const computerSelection = getComputerChoice();
+
+/*function playRound(humanChoice, computerChoice) {
+    if (humanChoice === computerChoice) {
+        console.log('its a tie');
+    } else if (humanChoice === 'rock' && computerChoice === 'scissors') {
+        playerScore++;
+        console.log(`Player Wins. ${humanChoice} beats ${computerChoice}`);
+    } else if (humanChoice === 'paper' && computerChoice === 'rock') {
+        playerScore++;
+        console.log(`Player Wins. ${humanChoice} beats ${computerChoice}`);
+    } else if (humanChoice === 'scissors' && computerChoice === 'paper') {
+        playerScore++;
+        console.log(`Player Wins. ${humanChoice} beats ${computerChoice}`);
+    } else {
+        computerScore++;
+        console.log(`Computer wins. ${computerChoice} beats ${humanChoice}.`);
+    }
+}*/
+
+function playGame(humanChoice, computerChoice) {
+    if (humanChoice === computerChoice) {
+        console.log('its a tie');
+    } else if (humanChoice === 'rock' && computerChoice === 'scissors') {
+        playerScore++;
+        console.log(`Player Wins. ${humanChoice} beats ${computerChoice}`);
+    } else if (humanChoice === 'paper' && computerChoice === 'rock') {
+        playerScore++;
+        console.log(`Player Wins. ${humanChoice} beats ${computerChoice}`);
+    } else if (humanChoice === 'scissors' && computerChoice === 'paper') {
+        playerScore++;
+        console.log(`Player Wins. ${humanChoice} beats ${computerChoice}`);
+    } else {
+        computerScore++;
+        console.log(`Computer wins. ${computerChoice} beats ${humanChoice}.`);
+    }
+}
+
+playGame()
+playGame()
+playGame()
+playGame()
+playGame()
