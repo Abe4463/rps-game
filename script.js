e0b1a9cfe2ad48e83c4a4eb7cd40ff6ffe96c1c5
Abe4463 <abrahamle124@gmail.com>
2025-05-29 09:31:58 -0700

    added computerchoice function

---
+++ script.js
@@ -0,0 +1,11 @@
+function getComputerChoice() {
+  const compChoices = ["rock", "paper", "scissors"];
+  const choice = Math.floor(Math.random() * compChoices.length);
+  return compChoices[choice];
+}
+
+console.log(getComputerChoice());
+
+function getHumanChoice() {
+  d;
+}
